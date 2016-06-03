ae14a6fe21c71bacd8b5d5edf5234eb09166285e
Ankush Sharma <ankprashar@gmail.com>
2016-06-03 16:22:27 +0530

    Update : Pep8 fixes

--- autorel/clients/github_client.py
+++ autorel/clients/github_client.py
@@ -1,19 +1,34 @@
-import base64, re
+import base64
+import re
 from http_client import HTTPClient
-from github_exceptions import *
+from github_exceptions import (BadCredentialsException,
+                               GithubException,
+                               UserAgentException,
+                               ObjectNotFoundException,
+                               UnprocessableEntityException,
+                               TwoFactorAuthException,
+                               LoginAttemptExceededException,
+                               BadRequestException,
+                               RateLimitExceededException
+                               )
+
 
 class Github:
+
     '''
         Client interacting with the Github REST API
     '''
-    def __init__(self,endpoint,port,token_or_login,password,clientID,clientSecret,userAgent,timeout):
+
+    def __init__(self, endpoint, port, token_or_login, password, clientID,
+                 clientSecret, userAgent, timeout):
         self._authorizationHeaders = {}
-        self._params = {} # URL-parameters
+        self._params = {}  # URL-parameters
         if password is not None:
             # https://developer.github.com/v3/#basic-authentication
             username = token_or_login()
             auth_string = username + ':' + password
-            self._authorizationHeaders = "Basic " + base64.b64encode(auth_string.encode('utf-8')).decode('utf-8')
+            self._authorizationHeaders = "Basic " + \
+                base64.b64encode(auth_string.encode('utf-8')).decode('utf-8')
         elif password is None:
             # https://developer.github.com/v3/#oauth2-token-sent-in-a-header
             self._authorizationHeaders = "token " + token_or_login
@@ -21,25 +36,31 @@ class Github:
             # https://developer.github.com/v3/#oauth2-keysecret
             self._params["client_id"] = clientID
             self._params["client_secret"] = clientSecret
-        # Github API requires a user agent, if provided else fallback to what HTTPClient is using
+        # Github API requires a user agent, if provided else fallback to what
+        # HTTPClient is using
         if userAgent:
             self.headers['User-Agent'] = userAgent
-        # Accept only version 3 of the API ( https://developer.github.com/v3/#current-version )
+        # Accept only version 3 of the API
+        # ( https://developer.github.com/v3/#current-version )
         self.headers['Accept'] = 'application/vnd.github.v3+json'
-        self._client = HTTPClient(endpoint,port,self._headers,self._params,timeout)
+        self._client = HTTPClient(
+            endpoint, port, self._headers, self._params, timeout)
 
-    def _doExceptionHandling(self,status_code,headers,data):
+    def _doExceptionHandling(self, status_code, headers, data):
         '''
             Check the response for possible exceptions
         '''
         exception_class = None
         if status_code == 401 and data.get("message") == 'Bad credentials':
             exception_class = BadCredentialsException
-        elif status_code == 403 and data.get("message").startswith('Maximum number'):
+        elif (status_code == 403 and
+              data.get("message").startswith('Maximum number')):
             exception_class = LoginAttemptExceededException
-        elif status_code == 403 and data.get("message").startswith("API Rate Limit Exceeded"):
+        elif (status_code == 403 and
+              data.get("message").startswith("API Rate Limit Exceeded")):
             exception_class = RateLimitExceededException
-        elif status_code == 403 and data.get("message").startswith("Missing or invalid User Agent string"):
+        elif (status_code == 403 and
+              data.get("message").startswith("Missing or invalid User Agent")):
             exception_class = UserAgentException
         elif status_code == 404 and data.get("message") == "Not Found":
             exception_class = ObjectNotFoundException
@@ -47,24 +68,20 @@ class Github:
             exception_class = BadRequestException
         elif status_code == 422:
             exception_class = UnprocessableEntityException
-        elif status == 401 and 'X-GitHub-OTP' in headers and re.match(r'.*required.*', headers['X-GitHub-OTP']):
+        elif (status_code == 401 and 'X-GitHub-OTP' in headers and
+              re.match(r'.*required.*', headers['X-GitHub-OTP'])):
             exception_class = TwoFactorAuthException
-        else:
+        elif str(status_code).startswith('4'):
             exception_class = GithubException
+        else:
+            return
+        raise exception_class(status_code, data)
 
-        return exception_class(status_code,data)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    def req(self, method='GET', url='', headers={}, params={}, payload={}):
+        '''
+            Make a request using the HTTPClient instance
+        '''
+        (status_code, headers, data) = self._client.makeRequest(
+            method, url, headers, params, payload)
+        self._doExceptionHandling(status_code, headers, data)
+        return GithubObject(status_code, headers, data)
