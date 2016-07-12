401b1b2f445ca05cb1e4034220905cb7b1e4390c
Ankush Sharma <ankprashar@gmail.com>
2016-07-12 23:09:47 +0530

    Restructured parser for dependency injection in ChangelogGenerator

--- autorel/changelog_generator/main/repository_parser.py
+++ autorel/changelog_generator/main/repository_parser.py
@@ -1,33 +1,51 @@
-## Helper classes for parsing pull requests ids, issues ids etc.
+"""
+    @module repository_parser
+    @class RepositoryParser
+    - All child parsers must register here
+"""
 
-class Parser(object):
+
+class RepositoryParser(object):
     """
-        Helper class to parse message bodies for pull request
-        and issues ids
+        Entry point for child parsers
     """
-    def __init__(self, supportedTypes):
-        self._supportedTypes = supportedTypes
-        self._parsers = {}
-        for type_ in self._supportedTypes:
-            # a list of functions to parse type_ messages
-            self._parsers[type_] = []
+    __pull_id_parser = None
+    __merged_branch_parser = None
+    __issue_id_parser = None
 
-    def addParser(self, parserType, function):
-        if parserType in self._supportedTypes:
-            self._parsers[parserType].append(function)
-        else:
-            raise ValueError('{0} pattern type not supported'.format(parserType))
+    @classmethod
+    def set_pull_id_parser(cls, parser):
+        cls.__pull_id_parser = parser
 
-    def parse(self, patternType, msg_body):
-        """
-            Parses a message body using the available parsers
-            Return value may be
-            - a pull request id
-            - an issue id
-            - a branch name
-        """
-        for parsing_func in self._parsers[patternType]:
-            parser_output = parsing_func(msg_body)
-            if parser_output:
-                parsed_value, type_ = parser_output
-                return parser_output
+    @classmethod
+    def set_merged_branch_parser(cls, parser):
+        cls.__merged_branch_parser = parser
+
+    @classmethod
+    def set_issue_id_parser(cls, parser):
+        cls.__issue_id_parser = parser
+
+    def parse_pull_id(self, input_text):
+        parser_obj = self.__pull_id_parser(input_text)
+        parser_obj.parse()
+        result = parser_obj.result
+        if len(result) > 1:
+            raise Exception('Repository Parser: Invalid input_text')
+        elif result:
+            return result[0]
+
+
+    def parse_merged_branch(self, input_text):
+        parser_obj = self.__merged_branch_parser(input_text)
+        parser_obj.parse()
+        result = parser_obj.result
+        if len(result) > 1:
+            raise Exception('Repository Parser: Invalid input_text')
+        elif result:
+            return result[0]
+
+    def parse_issue_id(self, input_text):
+        parser_obj = self.__issue_id_parser(input_text)
+        parser_obj.parse()
+        # Multiple issues can be fixed at a time
+        return parser_obj.result
