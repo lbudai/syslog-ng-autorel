5663ced2bd4594bfa46a4312ca160f34c2e8db1e
Ankush Sharma <ankprashar@gmail.com>
2016-07-12 23:07:21 +0530

    Fixed super and regex syntax

--- autorel/changelog_generator/parsers/id_parsers.py
+++ autorel/changelog_generator/parsers/id_parsers.py
@@ -1,10 +1,10 @@
 """
+    @module id_parsers
+    @classes PullIDParser, MergedBranchParser, IssueIDParser
     Implement any parsers here
-    @module main
-    @classes PullIDParser, PullBranchParser, IssueIDParser
 """
 import re
-from ..main import TokenParser
+from changelog_generator.main import TokenParser
 
 
 class PullIDParser(TokenParser):
@@ -14,9 +14,9 @@ class PullIDParser(TokenParser):
         => pull_id : 1234
     """
     def __init__(self, commit_msg):
-        super(self, PullIDParser).__init__("pull_id",
+        super(PullIDParser, self).__init__("pull_id",
                                            "Parses pull request id from commit message"
-                                            )
+                                           )
         self._set_input(commit_msg)
 
     def parse(self):
@@ -27,16 +27,16 @@ class PullIDParser(TokenParser):
                                   )
 
 
-class PullBranchParser(TokenParser):
+class MergedBranchParser(TokenParser):
     """
         Parses branch name from commit message
         => msg : Merge from user/master
         => branch_name : user/master
     """
     def __init__(self, commit_msg):
-        super(self, PullBranchParser).__init__("merged_branch",
-                                               "Parses pull request id from commit message"
-                                               )
+        super(MergedBranchParser, self).__init__("merged_branch",
+                                                 "Parses pull request id from commit message"
+                                                 )
         self._set_input(commit_msg)
 
     def parse(self):
@@ -47,7 +47,7 @@ class PullBranchParser(TokenParser):
                                   )
 
 
-class IssueIDParser(object):
+class IssueIDParser(TokenParser):
     """
         Parses the issue id from commit message or
         pull request body
@@ -55,13 +55,14 @@ class IssueIDParser(object):
         => issue_id : 1234
     """
     def __init__(self, msg):
-        super(self, PullBranchParser).__init__("issue_ids",
-                                               "Parses pull request id from commit message"
-                                               )
+        super(IssueIDParser, self).__init__("issue_id",
+                                            "Parses issue ids from commit message or\
+                                            pull request body"
+                                            )
         self._set_input(msg)
 
     def parse(self):
-        regexp = r"fixe?s?\s+#(\d+)(?=\b)"
+        regexp = r"fixe?s?\s+#(\d+)"
         self._result = re.findall(regexp,
                                   self._input,
                                   re.IGNORECASE)
