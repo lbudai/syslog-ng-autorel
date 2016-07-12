28c8b3e9d33a83e77b544f2e0c75a1bab3f09dfe
Ankush Sharma <ankprashar@gmail.com>
2016-07-12 23:06:32 +0530

    Added custom settings options

--- autorel/changelog_generator/settings.py
+++ autorel/changelog_generator/settings.py
@@ -1,5 +1,28 @@
-## Settings
+"""
+    @module settings
+    Configuration options for changelog generator
+"""
+from changelog_generator.fetchers import GithubFetcher
+from changelog_generator.parsers import (PullIDParser,
+                                         MergedBranchParser,
+                                         IssueIDParser
+                                         )
 
-AUTH_TOKEN = ""
+
+FETCHER_PLUGIN = GithubFetcher
+PULL_ID_PARSER = PullIDParser
+MERGED_BRANCH_PARSER = MergedBranchParser
+ISSUE_ID_PARSER = IssueIDParser
+
+# Changelog Categories
+ENHANCEMENT = "Enhancement"
+BUGFIX = "BugFix"
+FIXED_ISSUE = "Fixed Issue"
+MERGED_PULL = "Merged Pull"
+# Fetcher issue labels
+BUG_LABEL = "bug"
+ENHANCEMENT_LABEL = "enhancement"
+
+FETCHER_AUTH_TOKEN = ""
 PROJECT = "balabit/syslog-ng"
 CLONE_URL = "git://github.com/balabit/syslog-ng.git"
