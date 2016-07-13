5cf3216d6e430d10e35e5a5e546f9537f9533024
Ankush Sharma <ankprashar@gmail.com>
2016-07-13 20:53:47 +0530

    Refactored code using classes

--- autorel/changelog_generator/parsers/id_parsers.py
+++ autorel/changelog_generator/parsers/id_parsers.py
@@ -1,55 +1,67 @@
-## Register your parsing functions here
-
+"""
+    Implement any parsers here
+    @module main
+    @classes PullIDParser, PullBranchParser, IssueIDParser
+"""
 import re
+from ..main import TokenParser
 
 
-def pull_id_parser(msg_body):
+class PullIDParser(TokenParser):
     """
         Parses pull request id from commit message
         => msg : Merge pull request #1234 from username/branch
         => pull_id : 1234
     """
-    if msg_body.startswith('Merge pull'):
-        tokens = msg_body.split()
-        pull_id = tokens[3][1:]
-        return pull_id, "pull_id"
+    def __init__(self, commit_msg):
+        super(self, PullIDParser).__init__("pull_id",
+                                           "Parses pull request id from commit message"
+                                            )
+        self._set_input(commit_msg)
+
+    def parse(self):
+        regexp = r"merge\s+pull\s+request\s+#(\d+)"
+        self._result = re.findall(regexp,
+                                  self._input,
+                                  re.IGNORECASE
+                                  )
 
 
-def pull_branch_parser(msg_body):
+class PullBranchParser(TokenParser):
     """
         Parses branch name from commit message
         => msg : Merge from user/master
         => branch_name : user/master
     """
-    if msg_body.startswith('Merge branch'):
-        tokens = msg_body.split()
-        pull_branch = tokens[2]
-        return pull_branch, "merged_branch"
+    def __init__(self, commit_msg):
+        super(self, PullBranchParser).__init__("merged_branch",
+                                               "Parses pull request id from commit message"
+                                               )
+        self._set_input(commit_msg)
+
+    def parse(self):
+        regexp = r"merge\s+branch\s+(\S+)"
+        self._result = re.findall(regexp,
+                                  self._input,
+                                  re.IGNORECASE
+                                  )
 
 
-def issue_id_parser(msg_body):
+class IssueIDParser(object):
     """
-        Parses the issue id from commit message
+        Parses the issue id from commit message or
+        pull request body
         => msg : Fixes #1234
         => issue_id : 1234
-        It can also be used to parse pull request body for
-        the linked issue.
     """
-    result1 = re.findall(r"fix\s+#(\d+)(?=\b)",msg_body,re.IGNORECASE)
-    result2 = re.findall(r"fixes\s+#(\d+)(?=\b)",msg_body,re.IGNORECASE)
-    issue_ids = list(set(result1).union(set(result2)))
-    if len(issue_ids):
-        return issue_ids, "issue_ids"
+    def __init__(self, msg):
+        super(self, PullBranchParser).__init__("issue_ids",
+                                               "Parses pull request id from commit message"
+                                               )
+        self._set_input(msg)
 
-
-PARSING_FUNCTIONS = {
-    "merges": [
-        pull_id_parser,
-        pull_branch_parser
-    ],
-    "issues": [
-        issue_id_parser
-    ],
-}
-
-__all__ = [PARSING_FUNCTIONS]
+    def parse(self):
+        regexp = r"fixe?s?\s+#(\d+)(?=\b)"
+        self._result = re.findall(regexp,
+                                  self._input,
+                                  re.IGNORECASE)
