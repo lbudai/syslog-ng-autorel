4b0f610f43f9220c26e3087b03eb127a64a9814a
Ankush Sharma <ankprashar@gmail.com>
2016-08-01 22:43:50 +0530

    Facilitate EntryRenderer importing

--- autorel/changelog_generator/main/__init__.py
+++ autorel/changelog_generator/main/__init__.py
@@ -1,6 +1,7 @@
 from .repository_parser import RepositoryParser
 from .token_parser import TokenParser
 from .fetcher import Fetcher
+from .entry_renderer import EntryRenderer
 from .changelog_gen import (ChangelogEntry,
                             ChangelogGenerator
                             )
