5046b7d45ed0759ed4bf03710b34c779353f6e7c
Ankush Sharma <ankprashar@gmail.com>
2016-08-06 02:08:12 +0530

    add_author, create_branch methods added and some minor fixes

--- autorel/release/platform.py
+++ autorel/release/platform.py
@@ -1,41 +1,55 @@
 """
-	@module platform
-	@class Platform
-	- Abstracts out the platform dependence of the release cycle
-	- All platfroms such as Github, Gitlab etc. comply with the API
+    @module platform
+    @class Platform
+    - Abstracts out the platform dependence of the release cycle
+    - All platfroms such as Github, Gitlab etc. comply with the API
 """
-from github import Github
+import datetime
+from github import (Github,
+                    InputGitTreeElement,
+                    InputGitAuthor
+                    )
 from .settings import GITHUB_AUTH_TOKEN
 
 
-
 class Platform(object):
-	def __init__(self, project):
-		self._project = project
-		self._client = Github(GITHUB_AUTH_TOKEN)
-		self._repo = self._client.get_repo(self._project)
+    """
+        Exposes higher level platform API
+    """
+    def __init__(self, project):
+        self._project = project
+        self._client = Github(GITHUB_AUTH_TOKEN)
+        self._repo = self._client.get_repo(self._project)
 
-	def get_current_release():
-		"""
-			Get the latest release tag
-		"""
-		releases = []
-		for release in self._repo.get_releases():
-			releases.append(release)
-		current_release = releases[0]
-		return current_release.tag_name
+    def add_committer(self, name, email):
+        """
+            Create a git committer/author
+        """
+        date = datetime.datetime.utcnow().isoformat()
+        self._committer = InputGitAuthor(name=name,
+                                         email=email,
+                                         date=date
+                                         )
 
-	def create_new_branch(orig_branch, new_branch):
-		"""
-			Creates a new branch from the orig_branch
-		"""
-		if not orig_branch.startwith("heads/"):
-			orig_branch = "heads/" + orig_branch
-		if not new_branch.startwith("refs/heads/"):
-			new_branch = "refs/heads/" + new_branch
-		# Get the SHA value of the latest commit on the orig_branch
-		orig_branch_ref = self._repo.get_git_ref(orig_branch)
-		latest_commit_sha = orig_branch_ref.object.sha 
-		self._repo.create_git_ref(new_branch,latest_commit_sha)
+    def get_current_release(self):
+        """
+            Get the latest release tag
+        """
+        releases = []
+        for release in self._repo.get_releases():
+            releases.append(release)
+        current_release = releases[0]
+        return current_release.tag_name
 
-	
+    def create_new_branch(self, orig_branch, new_branch):
+        """
+            Creates a new branch from the orig_branch
+        """
+        if not orig_branch.startwith("heads/"):
+            orig_branch = "heads/" + orig_branch
+        if not new_branch.startwith("refs/heads/"):
+            new_branch = "refs/heads/" + new_branch
+        # Get the SHA value of the latest commit on the orig_branch
+        orig_branch_ref = self._repo.get_git_ref(orig_branch)
+        latest_commit_sha = orig_branch_ref.object.sha
+        self._repo.create_git_ref(new_branch,latest_commit_sha)
