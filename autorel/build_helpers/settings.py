fff88014b5794255ee8b33414dcbd9dc7cc2d3dc
Ankush Sharma <ankprashar@gmail.com>
2016-08-16 03:28:39 -0400

    Fixed old wildcards and added some new ones

--- autorel/build_helpers/settings.py
+++ autorel/build_helpers/settings.py
@@ -4,18 +4,20 @@
 """
 import os
 
+## Path of the build direcory within the source ##
+BUILD_DIRECTORY_SUBPATH = "build"
 
 ## Path of the docker image used for building debian source ##
 DEBIAN_SOURCE_BUILDING_IMAGE = os.path.abspath("../dockerfiles/debian-source-build")
 
 ## Wildcard used fo matching the orig tarball file ##
-ORIG_TARBALL_FILE_WILDCARD = "*ori.tar.gz"
+ORIG_TARBALL_FILE_WILDCARD = "*orig.tar.gz"
 
 ## Wildcard used for matching the quilt format based patch file ##
 PATCH_FILE_WIDLCARD = "*.tar.xz"
 
 ## Wildcard used for matching the source control files(.dsc) ##
-SOURCE_CONTROL_FILE_WILDCARD = "*.tar.dsc"
+SOURCE_CONTROL_FILE_WILDCARD = "*.dsc"
 
 ## Wildcard used for matching the path of the source directory ##
 SOURCE_DIRECTORY_WILDCARD = "syslog-ng-*"
@@ -24,4 +26,13 @@ SOURCE_DIRECTORY_WILDCARD = "syslog-ng-*"
 SOURCE_TARBALL_BUILDING_IMAGE = os.path.abspath("../dockerfiles/source-tarball-build")
 
 ## Wildcard used for matching the path of the source tarball ##
-TARBALL_FILE_WILDCARD = "syslog-ng_*"
+TARBALL_FILE_WILDCARD = "syslog-ng-*.tar.gz"
+
+## The Source directory ##
+SOURCE_DIRECTORY = "syslog-ng"
+
+## Version ##
+VERSION = "3.8.0alpha0"
+
+## Original tarball extension ##
+ORIG_TARBALL_FILE_FORMAT = "syslog-ng_{0}.orig.tar.gz"
