72562122d5f47230ec775a300a65c706e7858c5d
Ankush Sharma <ankprashar@gmail.com>
2016-08-16 03:31:14 -0400

    Builder fixes

--- autorel/build_helpers/utils.py
+++ autorel/build_helpers/utils.py
@@ -4,17 +4,19 @@
     source tarballs and debian source packages
 """
 import os
-from build_helpers.settings import (DEBIAN_SOURCE_BUILDING_IMAGE,
-                               ORIG_TARBALL_FILE_WILDCARD,
-                               PATCH_FILE_WIDLCARD,
-                               SOURCE_CONTROL_FILE_WILDCARD,
-                               SOURCE_DIRECTORY_WILDCARD,
-                               SOURCE_TARBALL_BUILDING_IMAGE,
-                               TARBALL_FILE_WILDCARD
-                               )
+import glob
+from build_helpers.settings import (ORIG_TARBALL_FILE_WILDCARD,
+                                    PATCH_FILE_WIDLCARD,
+                                    SOURCE_CONTROL_FILE_WILDCARD,
+                                    SOURCE_DIRECTORY_WILDCARD,
+                                    TARBALL_FILE_WILDCARD,
+                                    BUILD_DIRECTORY_SUBPATH,
+                                    SOURCE_DIRECTORY,
+                                    ORIG_TARBALL_FILE_FORMAT,
+                                    VERSION
+                                    )
 
 
-    
 class DebianSourcePackage(object):
     """
         Debian source package abstraction
@@ -37,27 +39,35 @@ class DebianSourcePackage(object):
         return self._source_control_file
 
 
-def get_debian_source_building_commands(source_location):
+def get_debian_source_building_commands(input_directory):
     """
         Returns the list of commands to be executed to
         build the debian source package
     """
+    orig_tarball_name = ORIG_TARBALL_FILE_FORMAT.format(VERSION)
     commands = [
-        "(cd {0} && tar -xvf {1})".format(source_location,
+        "(cd {0} && tar -xvf {1})".format(input_directory,
                                           TARBALL_FILE_WILDCARD
                                           ),
-        "(cd {0} && dpkg-source -Us -Uc -b {1}".format(source_location,
+        "(cd {0} && mv {1} {2})".format(input_directory,
+                                       TARBALL_FILE_WILDCARD,
+                                       orig_tarball_name
+                                       ),
+        "(cd {0} && dpkg-source -Us -Uc -b {1})".format(input_directory,
                                                        SOURCE_DIRECTORY_WILDCARD
                                                        )
     ]
     return commands
 
 
-def get_source_tarball_building_commands(source_location):
+def get_source_tarball_building_commands(input_directory):
     """
         Returns the list of commands to be executed to
         build the distribution tarball
     """
+    source_location = os.path.join(input_directory,
+                                   SOURCE_DIRECTORY
+                                   )
     commands = [
             "(cd {0} && pip install -r requirements.txt)",
             "(cd {0} && ./autogen.sh)",
@@ -65,33 +75,37 @@ def get_source_tarball_building_commands(source_location):
             "(cd {0}/build && ../configure --enable-debug)",
             "(cd {0}/build && make distcheck)"
     ]
-    for command in commands:
-        command.format(source_location)
+    for index in range(len(commands)):
+        commands[index] = commands[index].format(source_location)
     return commands
 
-    
 def debian_source_transformer(input_directory):
     """
         Returns a DebianSourcePackage instance
     """
     lookup_directory = input_directory
     os.chdir(lookup_directory)
-    linked_tarball = glob.glob(ORIG_TARBALL_LOCATION)[0]
+    linked_tarball = glob.glob(ORIG_TARBALL_FILE_WILDCARD)[0]
+    linked_tarball = os.path.abspath(linked_tarball)
     patch_file = glob.glob(PATCH_FILE_WIDLCARD)[0]
+    patch_file = os.path.abspath(patch_file)
     source_control_file = glob.glob(SOURCE_CONTROL_FILE_WILDCARD)[0]
+    source_control_file = os.path.abspath(source_control_file)
     source_package = DebianSourcePackage(linked_tarball,
                                          patch_file,
                                          source_control_file
                                          )
     return source_package
 
-    
 def source_tarball_transformer(input_directory):
     """
         Returns the path of source tarball by looking into
         the input_directory
     """
-    lookup_directory = input_directory 
+    lookup_directory = os.path.join(input_directory,
+                                    SOURCE_DIRECTORY,
+                                    BUILD_DIRECTORY_SUBPATH
+                                    )
     os.chdir(lookup_directory)
     distribution_tarball = glob.glob(TARBALL_FILE_WILDCARD)[0]
     distribution_tarball_path = os.path.abspath(distribution_tarball)
